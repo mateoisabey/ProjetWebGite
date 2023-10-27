11336210ce8d4ba96bbaffeb2d5a7ae0859187d0
Mateo Isabey <mateoisabey@gmail.com>
2023-10-27 14:39:28 +0200

    modif de la map

--- javascript/map.js
+++ javascript/map.js
@@ -1,7 +1,6 @@
 var map = L.map('map').setView([44.44898830120307, 2.493317205079201], 14);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([44.44898830120307, 2.493317205079201]).addTo(map)
