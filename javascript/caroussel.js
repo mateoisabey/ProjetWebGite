a198acb823a546bdf1768bedc234acce9265aacc
Mateo Isabey <mateoisabey@gmail.com>
2023-10-31 09:29:58 +0100

    lazy loading

--- javascript/caroussel.js
+++ javascript/caroussel.js
@@ -12,6 +12,20 @@ function changeSlide(n) {
     showSlide(currentSlide + n);
 }
 
+let observer;
+
+function createObserver() {
+    observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                const src = img.getAttribute('data-src');
+                img.setAttribute('src', src);
+                observer.unobserve(img);
+            }
+        });
+    }, { threshold: 0.5 });
+}
 
 async function loadImages() {
     const response = await fetch('php/get_images.php');
@@ -20,50 +34,50 @@ async function loadImages() {
 
         images.forEach(function (image) {
             const imgElement = document.createElement('img');
-            imgElement.src = 'data:image/jpeg;base64,' + image.image_data;
-            imgElement.style.display = 'none'; // Cachez toutes les images par défaut
+            imgElement.setAttribute('data-src', 'data:image/jpeg;base64,' + image.image_data);
+            imgElement.style.display = 'none';
             imageContainer.appendChild(imgElement);
             slides.push(imgElement);
-            imageIds.push(image.id); // Stockez les ID dans le tableau imageIds
+            imageIds.push(image.id);
+
+            observer.observe(imgElement);
         });
 
         if (slides.length > 0) {
-            slides[0].style.display = 'block'; // Affichez la première image
+            slides[0].style.display = 'block';
             currentSlide = 0;
         }
     } else {
         console.error('La requête AJAX a échoué.');
     }
 }
+
+
+createObserver();
+loadImages();
+
 function deleteImage() {
     if (slides.length > 0) {
         const imageToDelete = slides[currentSlide];
-        const imageId = imageIds[currentSlide]; // Récupérez l'ID de l'image actuellement affichée
+        const imageId = imageIds[currentSlide];
 
-        // Supprimez l'image du carrousel
         imageContainer.removeChild(imageToDelete);
         slides.splice(currentSlide, 1);
         imageIds.splice(currentSlide, 1);
 
-        // Effectuez une requête AJAX pour supprimer l'image de la base de données en utilisant imageId
         const xhr = new XMLHttpRequest();
         xhr.open("POST", "php/suppressionImage.php", true);
         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
-                // Suppression réussie dans la base de données
                 console.log("L'image avec l'ID " + imageId + " a été supprimée de la base de données.");
             }
         };
-
-        // Envoyez l'ID de l'image à supprimer
         const data = "image_id=" + imageId;
         xhr.send(data);
     }
 }
 
-
-loadImages();
 
 let i = 0;
 
